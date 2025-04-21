4e2c6c7b0fe27d03ea68f7efa08e1324d8043b78
PaulaPriotti <paaupriotti@gmail.com>
2025-04-21 19:16:42 -0300

    consegui traer datos
    se traen los datos necesarios de cada array de paises

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
-const api = 'https://restcountries.com';
+const api = 'https://restcountries.com/v3.1/all';
 
 fetch(api)
-.then(res => res.json())
-.then(data => {
-    console.log(data);
+  .then(res => res.json())
+  .then(data => {
+    console.log(data); // Esto ahora sí te va a dar un array de países
+    const datoPais = data.map(pais => {
+      return {
+        nombre: pais.name.common,
+        capital: pais.capital ? pais.capital[0] : 'No tiene capital',
+        bandera: pais.flags.png
+      };
+    });
+    console.log(datoPais); // Esto ahora sí te va a dar un array de países
 })
-.catch(err => {
+  .catch(err => {
     console.log('Error: ' + err);
 });
-// const api = 'https://restcountries.com/v3.1/all';
