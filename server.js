3b09a2b4cef36a6fbbe9f24cef3c7ae39e56cd4b
PaulaPriotti <paaupriotti@gmail.com>
2025-04-20 23:40:04 -0300

    se empieza con el server.js e instalaciones

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const http = require('http');
+
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
